461cd7db5e3d68f0b23f650158af3700d9dd2d93
xiaowoxiaowo <awlzluggeoo@sina.cn>
2020-06-10 16:50:36 +0800

    Virtual DOM介绍

--- my/2-数据驱动.js
+++ my/2-数据驱动.js
@@ -19,3 +19,10 @@
  * vue的_render是一个私有方法，用来把实例渲染成一个虚拟Node，定义在core/instance/render.js中
  * 在instance/lifecycle.js里的初始化里把_render方法添加到响应式依赖中，通过渲染watcher来触发updateComponent的_render方法
  */
+
+/***
+ * Virtual DOM
+ * 用原生的js对象去描述一个DOM节点，vue中用VNode这样一个类去描述Virtual DOM, 定义在core/vdom/vnode.js中
+ * VNode需要经过create,diff,patch等过程才能映射到真实的DOM上
+ */
+
